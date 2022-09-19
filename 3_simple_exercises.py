74106a5562908fe3d6f01a78dd37de8245adae08
Thomas <thomas_zelazny1@baylor.edu>
2022-09-18 21:29:23 -0500

    word frequency change

--- 3_simple_exercises.py
+++ 3_simple_exercises.py
@@ -12,6 +12,7 @@ sampleDict = {
       }
    }
 }
+print(sampleDict["class"]["student"]["marks"]["history"])
 
 
 
@@ -20,6 +21,9 @@ sampleDict = {
 
 dict={"son's name": "Lucas", "son's eyes": "green", "son's height": 32, "son's weight": 25}
 
+dict["son's height"]+=2
+
+print(dict)
 
 
 
@@ -32,6 +36,9 @@ sampleDict = {
      'emp3': {'name': 'Brad', 'salary': 6500}
 }
 
+sampleDict["emp3"]["salary"]=8500
+
+#print(sampleDict)
 
 
 
@@ -42,4 +49,6 @@ sampleDict = {
 
 dict={"name": "Plato", "country": "Ancient Greece", "born": -427, "teacher": "Socrates", "student": "Aristotle"}
 
+dict["work"]=["Apology", "Phaedo", "Republic", "Symposium"]
 
+#print(dict)
