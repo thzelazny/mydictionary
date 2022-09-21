c0d32f3b0bac8a84358566548817c2717e99f355
Thomas <thomas_zelazny1@baylor.edu>
2022-09-20 22:01:17 -0500

    file change

--- FileEncryption.py
+++ FileEncryption.py
@@ -1,4 +1,4 @@
-outfile=open("sometext.txt","r")
+outfile=open("info_security.txt","r")
 infile=open("encrypted.txt","w")
 
 encryption=dict(a="1",A="!",b="2",B="@",c="3",C="#",d="4",D="$",e="5",E="%",f="6",F="^",g="7",G="&",h="8",H="*",i="9",I="(",j="0",J=")",k="[",K="{",l="]",L="}",m="|",M=":",n=";",N="'",o='"',O=",",p="D",P="<",q=".",Q=">",r="-",R="_",s="+",S="=",t="?",T="/",u="`",U="~",v="r",V="R",w="e",W="E",x="l",X="L",y="o",Y="O",z="n",Z="N")
